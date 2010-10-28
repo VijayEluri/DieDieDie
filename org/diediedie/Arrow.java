3db727a6fbfc561545557decc912389660f94374
beltsonata <sean.talbot@gmail.com>
2010-10-28 12:17:59 +0100

    gravity sort-of being applied to Arrows now. angle not being updated yet

--- org/diediedie/Arrow.java
+++ org/diediedie/Arrow.java
@@ -13,14 +13,17 @@ public class Arrow
 {
     private float startX, startY, endX, endY, accelX = 0, accelY = 0, 
                   speedX = 0, speedY = 0, mouseX, mouseY, angle, 
-                  moveSpeed = 0.4f;
+                  moveSpeed = 0.4f, gravity = 0;
                   
-    private final float SIZE = 20f, AIR_REST = 0.15f, ACCEL_RATE = 0.1f;
+    private final float SIZE = 20f, AIR_REST = 0.15f, ACCEL_RATE = 0.1f,
+                  MAX_GRAVITY = 20.4f, GRAVITY_INCR = 0.1f;
+                    
+    private final int DOWN = 180;
     private Level level = null;
     private Color color = Color.red;
     private boolean isFlying = false;
-
     
+   
     /**
      * Creates a new arrow at the given position.
      */ 
@@ -71,29 +74,19 @@ public class Arrow
      */
     private void setAngle(float x, float y)
     {
-        angle = (float)Math.toDegrees(Math.atan2(x - startX,
-                                                 startY - y));                                                                       
+        angle = (float)Math.toDegrees(Math.atan2(x - startX, startY - y));                                                                       
     }
     
     /**
      * Updates the speed of a release arrow
      */ 
     protected void updateSpeed()
-    {        
-        decelerate();    
+    {          
         speedX = (moveSpeed * accelX);
         speedY = (moveSpeed * accelY);
     }
     
     
-    private void decelerate()
-    {
-        /*accelX -= ACCEL_RATE;
-        if(accelX < 0)
-        {
-            accelX = 0;
-        }*/
-    }
     
     /**
      * Updates the position of the Arrow on the screen after it has left
@@ -108,21 +101,26 @@ public class Arrow
         System.out.println("\t" + "speedX: " + speedX + ", speedY: "
                            + speedY);*/
         
+        // move in the @angle direction
         startX = (float)(startX + xTravelled * 
                             Math.sin(Math.toRadians(angle)));
         startY = (float)(startY - yTravelled * 
                             Math.cos(Math.toRadians(angle)));   
-        //applyGravity()       
+        applyGravity();       
         calculateEndPos();
     }
 
     /*
-     * Applies 'gravity' to the angle and yPos
+     * Applies 'gravity' to the vertical position
      */ 
     private void applyGravity()
     {
-        startY += speedY * level.gravity;
+        startY += gravity;
         
+        if(gravity < MAX_GRAVITY)
+        {
+            gravity += GRAVITY_INCR;
+        }
     }
     
     /*
@@ -133,15 +131,6 @@ public class Arrow
     {
         endX = (float)(startX + SIZE * Math.sin(Math.toRadians(angle)));
         endY = (float)(startY - SIZE * Math.cos(Math.toRadians(angle)));
-    }
-    
-
-    /*
-     * Subtracts from speed to simulate air resistance. 
-     */ 
-    private void applyAirRest()
-    {
-        
     }
     
     /**
